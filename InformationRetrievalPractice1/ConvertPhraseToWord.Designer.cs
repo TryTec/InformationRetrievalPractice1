4d956d5e86c6331084df04eac3983018ca85e2c9
Try <trydev1995@gmail.com>
2024-02-03 09:37:03 +0700

    Add project files.

---
+++ InformationRetrievalPractice1/ConvertPhraseToWord.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace InformationRetrievalPractice1
+{
+    partial class ConvertPhraseToWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnUpload = new System.Windows.Forms.Button();
+            this.TxtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnUpload
+            // 
+            this.BtnUpload.Location = new System.Drawing.Point(4, 4);
+            this.BtnUpload.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnUpload.Name = "BtnUpload";
+            this.BtnUpload.Size = new System.Drawing.Size(112, 32);
+            this.BtnUpload.TabIndex = 0;
+            this.BtnUpload.Text = "Upload";
+            this.BtnUpload.UseVisualStyleBackColor = true;
+            this.BtnUpload.Click += new System.EventHandler(this.BtnUpload_Click);
+            // 
+            // TxtSource
+            // 
+            this.TxtSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TxtSource.Location = new System.Drawing.Point(3, 38);
+            this.TxtSource.Multiline = true;
+            this.TxtSource.Name = "TxtSource";
+            this.TxtSource.Size = new System.Drawing.Size(403, 395);
+            this.TxtSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(403, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TxtResult.Location = new System.Drawing.Point(412, 38);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtResult.Size = new System.Drawing.Size(403, 395);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(412, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(403, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Result";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.TxtSource, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TxtResult, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 40);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(818, 436);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BtnUpload);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(818, 40);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 476);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnUpload;
+        private System.Windows.Forms.TextBox TxtSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ InformationRetrievalPractice1/ConvertPhraseToWord.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InformationRetrievalPractice1
+{
+    public partial class ConvertPhraseToWord : Form
+    {
+        private readonly List<string> listFileName = new List<string>(); //For store uploaded files
+        private int docID = 0; //For store file id
+        private bool isFirstLine = true; //For check condition create column name in first line
+        public ConvertPhraseToWord()
+        {
+            InitializeComponent();
+            File.WriteAllText("result.txt", string.Empty); //For clear all text in result.txt
+        }
+
+        private void BtnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog(); //For Open File Dialog to select source text file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Check condition if user click on button OK or Open will get selected file
+            {
+                StreamReader reader = new StreamReader(openFileDialog1.FileName); //Read file from selected file
+                string line; //For store text line
+                if (!listFileName.Contains(openFileDialog1.SafeFileName)) //Check file name exist or not
+                {
+                    docID++; //Assign id
+                    listFileName.Add(openFileDialog1.SafeFileName); //Add file name with extension
+                    while ((line = reader.ReadLine()) != null) //Read text in file line by line
+                    {
+                        TxtSource.Text = line; //Assign text from line to TextBox for display source text
+                        string[] words = line.Split(' '); //Split phrase to word by space
+                        foreach (string word in words) //Get earch word in words array
+                        {
+                            using (StreamWriter writer = new StreamWriter("result.txt", true)) //Handle text file to write text into and append text
+                            {
+                                if (isFirstLine) //If first line with add these columns name
+                                {
+                                    writer.WriteLine("term\tdocID"); //Write these columns name
+                                    isFirstLine = false; //Set to false to skip Add columns name next line
+                                }
+                                writer.WriteLine(word + "\t" + docID); //Add word and docID to result.txt file
+                            }
+                        }
+                    }
+                    reader.Close(); //Clear memory
+
+                    reader = new StreamReader("result.txt"); //Read text from this file to display on TextBox result
+                    TxtResult.Text = reader.ReadToEnd(); //Display text from result.txt file to TextBox result
+                    reader.Close(); //Clear memory
+                }
+                else
+                {
+                    MessageBox.Show("Document already uploaded"); //Show when upload exist file name
+                }
+            }
+        }
+    }
+}
